05d2e639af91f0171d3e7669c6fc60744a4695d8
Anthony Castelli <anthonycastelli01@gmail.com>
2015-10-04 00:39:40 -0700

    Implement temporary addition of new tasks

--- js/app.js
+++ js/app.js
@@ -5,6 +5,15 @@
     this.tasks = exampleTasks;
   });
 
+  app.controller('TaskController', function() {
+    this.task = {};
+
+    this.addTask = function(list) {
+      list.tasks.push(this.task);
+      this.task = {};
+    }
+  });
+
   var exampleTasks = [
     {
       name: "Feed the dog"
